379c2d33d069ec0840da80bb8caacefb24921fcd
SL-207 <sl207020@gmail.com>
2025-01-31 21:00:24 +0000

    Add Test files

---
+++ starter/starter/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+from ml.data import process_data
+from train_model import cat_features, encoder, lb, loaded_model
+
+class ModelInput(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
+app = FastAPI()
+
+@app.get("/")
+async def greeting():
+    return {"greeting": "Greetings! The following app makes predictions on income range based on demographic and census-related factors"}
+
+@app.post("/inference")
+async def make_inference(data: ModelInput):
+    data_dict = data.dict()
+    data_dict = {key.replace("_", "-"): value for key, value in data_dict.items()}
+    df = pd.DataFrame([data_dict])
+    X, _, _, _ = process_data(df,
+                     categorical_features=cat_features,
+                     label=None,
+                     training=False,
+                     encoder=encoder,
+                     lb=lb)
+
+    output = loaded_model.predict(X)
+    if output == 1:
+        out_str = "Salary is over 50k"
+    elif output == 0:
+        out_str = "Salary is less than or equal to 50k"   
+    
+    return {"output": out_str}
---
+++ starter/starter/test_main.py
@@ -0,0 +1,53 @@
+from fastapi.testclient import TestClient
+import json
+
+from main import app
+
+client = TestClient(app)
+
+def test_greeting():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == {"greeting": "Greetings! The following app makes predictions on income range based on demographic and census-related factors"}
+    
+def test_inference_under_fifty():
+    data = json.dumps({
+        "age": 39,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education_num": 13,
+        "marital_status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital_gain": 2174,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "United-States"
+    })
+    r = client.post("/inference", data=data)
+    assert r.status_code == 200
+    assert r.json() == {"output": "Salary is less than or equal to 50k"}
+    
+def test_inference_over_fifty():
+    data = json.dumps({
+        "age": 43,
+        "workclass": "Federal-gov",
+        "fnlgt": 410867,
+        "education": "Doctorate",
+        "education_num": 16,
+        "marital_status": "Never-married",
+        "occupation": "Prof-specialty",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Female",
+        "capital_gain": 0,
+        "capital_loss": 0,
+        "hours_per_week": 50,
+        "native_country": "United-States"
+    })
+    r = client.post("/inference", data=data)
+    assert r.status_code == 200
+    assert r.json() == {"output": "Salary is over 50k"}
