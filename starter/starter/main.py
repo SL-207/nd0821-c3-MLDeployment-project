1cad949eb61ddaa00b3619fff6f9521157890364
SL-207 <sl207020@gmail.com>
2025-02-03 16:38:43 +0000

    Add screenshots

--- starter/starter/main.py
+++ starter/starter/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
 from starter.starter.ml.data import process_data
-from train_model import cat_features, encoder, lb, loaded_model
+from starter.starter.train_model import cat_features, encoder, lb, loaded_model
 
 
 class ModelInput(BaseModel):
